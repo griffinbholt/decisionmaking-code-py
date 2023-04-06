61a61c8d444bbc1f6bec45346bbba3ce2518f208
Griffin Holt <griffinbholt@gmail.com>
2023-04-06 11:37:50 -0700

    Fixed warnings in Chapter 4.

--- code/ch04.py
+++ code/ch04.py
@@ -6,18 +6,19 @@ from typing import Callable
 
 from ch02 import Variable
 
-"""
-A function for extracting the statistics, or counts, from a discrete data set `data`, 
-assuming a Bayesian network with `variables` and structure `graph`. 
-The data set `data` is an n x m matrix, where n is the number of variables and 
-m is the number of data points. This function returns an array M of length n. 
-The ith component consists of a q_i x r_i matrix of counts.
 
-ASSUMES DATA IS ZERO-INDEXED: instead of the entries begin classes {1, 2, 3}, they must be {0, 1, 2}
-
-Note: np.ravel_multi_index indexes the parental instantiations differently than sub2ind
-"""
 def statistics(variables: list[Variable], graph: nx.DiGraph, data: np.ndarray) -> list[np.ndarray]:
+    """
+    A function for extracting the statistics, or counts, from a discrete data set `data`,
+    assuming a Bayesian network with `variables` and structure `graph`.
+    The data set `data` is an n x m matrix, where n is the number of variables and
+    m is the number of data points. This function returns an array M of length n.
+    The ith component consists of a q_i x r_i matrix of counts.
+
+    ASSUMES DATA IS ZERO-INDEXED: instead of the entries begin classes {1, 2, 3}, they must be {0, 1, 2}
+
+    Note: np.ravel_multi_index indexes the parental instantiations differently than sub2ind
+    """
     n = len(variables)
     r = np.array([var.r for var in variables])
     q = np.array([int(np.prod([r[j] for j in graph.predecessors(i)])) for i in range(n)])
@@ -32,14 +33,17 @@ def statistics(variables: list[Variable], graph: nx.DiGraph, data: np.ndarray) -
             M[i][j, k] += 1.0
     return M
 
+
 def prior(variables: list[Variable], graph: nx.DiGraph) -> list[np.ndarray]:
     n = len(variables)
     r = [var.r for var in variables]
     q = np.array([int(np.prod([r[j] for j in graph.predecessors(i)])) for i in range(n)])
     return [np.ones((q[i], r[i])) for i in range(n)]
 
+
 def gaussian_kernel(b: float) -> Callable[[float], float]:
     return lambda x: norm.pdf(x, loc=0, scale=b)
+
 
 def kernel_density_estimate(kernel: Callable[[float | np.ndarray], float], observations: np.ndarray) -> Callable:
     return lambda x: np.mean([kernel(x - o) for o in observations])
