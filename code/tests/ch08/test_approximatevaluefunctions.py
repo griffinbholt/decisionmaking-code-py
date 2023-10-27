a62a666cdb37106a096b5bd0b0dac351757bce6f
Griffin Holt <griffinbholt@gmail.com>
2023-10-27 03:23:02 -0700

    Created stubs for Chapter 8 tests.

---
+++ code/tests/ch08/test_approximatevaluefunctions.py
@@ -0,0 +1,20 @@
+import sys; sys.path.append('./code/'); sys.path.append('../../')
+
+from ch08 import NearestNeighborValueFunction, LocallyWeightedValueFunction, MultilinearValueFunction, SimplexValueFunction, LinearRegressionValueFunction
+
+
+class TestApproximateValueFunctions():
+    def test_nearest_neighbor(self):
+        pass  # TODO
+
+    def test_locally_weighted(self):
+        pass  # TODO
+
+    def test_multilinear_interpolation(self):
+        pass  # TODO
+
+    def test_simplex_interpolation(self):
+        pass  # TODO
+
+    def test_linear_regression(self):
+        pass  # TODO
---
+++ code/tests/ch08/test_approximatevalueiteration.py
@@ -0,0 +1,8 @@
+import sys; sys.path.append('./code/'); sys.path.append('../../')
+
+from ch08 import *
+
+
+class TestApproximateValueIteration():
+    def test(self):
+        pass  # TODO
