c88fa228ddf8ffbb04eb85d542ec1ae6dc7a4342
Griffin Holt <griffinbholt@gmail.com>
2023-12-06 15:57:31 -0800

    Prepped Ch 10 test stub.

--- src/tests/ch10/test_monte_carlo_policy_eval.py
+++ src/tests/ch10/test_monte_carlo_policy_eval.py
@@ -1,7 +1,7 @@
 import sys; sys.path.append('./src/'); sys.path.append('../'); sys.path.append('../../')
 
-from ch07 import LinearQuadraticProblem
-from ch10 import MonteCarloPolicyEvaluation
+# from ch07 import LinearQuadraticProblem
+# from ch10 import MonteCarloPolicyEvaluation
 
 class TestMonteCarloPolicyEvaluation():
     def test(self):
